59977279a30f92728a963dd3c75ec00a700cc631
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-03-31 16:53:04 +0200

    Adding scss to webpack and creating sass files.

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log("działa");
+import "../scss/index.scss";
+
+const str = "Hello";
+
+const encodedStr = btoa(str);
+
+console.log(encodedStr);
+
+const encode =
+  "SmVzdCB0byBiYXJkem8gZmFqbnkgcHJvZ3JhbSBkbyBwcnplY2hvd2FuaWEgaSB1a3J5d2FuaWEgZGFueWNo";
+
+const str2 = atob(encodedStr);
+
+console.log(str2);
