bd2d615376195fd69c718b6301536ed7142f5fdf
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-04-01 19:50:33 +0200

    base64 is now working for encode string

--- js/app.js
+++ js/app.js
@@ -1,14 +1,39 @@
 import "../scss/index.scss";
+import Encode from "./encoding";
 
-const str = "Hello";
+// global variables
+const items = document.querySelectorAll('input[name="radio_btn"]');
+const textareaValue = document.getElementById("form-group__encoding");
+const btn = document.getElementById("btn");
+const div = document.querySelector(".history");
 
-const encodedStr = btoa(str);
+const getSelectionValue = () => {
+  let value;
 
-console.log(encodedStr);
+  for (let i = 0; i < items.length; i++) {
+    if (items[i].checked) {
+      value = items[i].value;
 
-const encode =
-  "SmVzdCB0byBiYXJkem8gZmFqbnkgcHJvZ3JhbSBkbyBwcnplY2hvd2FuaWEgaSB1a3J5d2FuaWEgZGFueWNo";
+      break;
+    }
+  }
 
-const str2 = atob(encodedStr);
+  return value;
+};
 
-console.log(str2);
+const base64 = () => {
+  switch (true) {
+    case getSelectionValue() == "encode":
+      const encode = new Encode(textareaValue.value);
+
+      div.appendChild(document.createTextNode(encode.text));
+
+      textareaValue.value = "";
+      break;
+  }
+};
+
+// listener
+(() => {
+  btn.addEventListener("click", base64);
+})();
