0158cc08c985e677e90565336534375893c23e23
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-04-05 20:49:07 +0200

    Now encode text also is located in local session

--- js/app.js
+++ js/app.js
@@ -6,7 +6,10 @@ import Decode from "./decoding";
 const items = document.querySelectorAll('input[name="radio_btn"]');
 const textareaValue = document.getElementById("form-group__textarea");
 const btn = document.getElementById("btn");
-const div = document.querySelector(".history");
+const history = document.querySelector(".history");
+const textareaText = document.querySelector(".form-group__label");
+// local session encode values
+let source;
 
 // this function return what option is selected from radio input
 const getSelectionValue = () => {
@@ -29,30 +32,55 @@ const base64 = () => {
     case getSelectionValue() == "encode":
       const encode = new Encode(textareaValue.value);
 
-      div.appendChild(document.createTextNode(encode.text));
+      // put in history block
+      history.appendChild(document.createTextNode(encode.text));
+
+      // set session storage encode item
+      if (sessionStorage.getItem("encode") === null) {
+        source = [];
+      } else {
+        source = JSON.parse(sessionStorage.getItem("encode"));
+      }
+
+      source.push(encode.text);
+      sessionStorage.setItem("encode", JSON.stringify(source));
 
       textareaValue.value = "";
+
       break;
     case getSelectionValue() == "decode":
       const decode = new Decode(textareaValue.value);
 
-      div.appendChild(document.createTextNode(decode.text));
+      history.appendChild(document.createTextNode(decode.text));
 
       textareaValue.value = "";
       break;
   }
 };
 
+// get items from local session
+const getItems = () => {
+  if (sessionStorage.getItem("encode") === null) return;
+
+  const sources = JSON.parse(sessionStorage.getItem("encode"));
+
+  sources.forEach((source) => {
+    history.appendChild(document.createTextNode(source));
+  });
+};
+
 // change the text according to the selected option
-const changeBtnContent = e => {
+const changeBtnContent = (e) => {
   const getSelection = e.target.value;
 
   switch (true) {
     case getSelection == "encode":
       btn.textContent = "Encode";
+      textareaText.textContent = "Encode the message";
       break;
     case getSelection == "decode":
       btn.textContent = "Decode";
+      textareaText.textContent = "Decode the message";
       break;
   }
 };
@@ -60,5 +88,7 @@ const changeBtnContent = e => {
 // listeners
 (() => {
   btn.addEventListener("click", base64);
-  [...items].map(item => item.addEventListener("change", changeBtnContent));
+  [...items].map((item) => item.addEventListener("change", changeBtnContent));
+  // load items from session if exist
+  document.addEventListener("DOMContentLoaded", getItems);
 })();
