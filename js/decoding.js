3caeffecbb833208ef1352f84b5229e1ef66a0f4
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-04-06 11:48:32 +0200

    Refactoring Decoding class

--- js/decoding.js
+++ js/decoding.js
@@ -1,12 +1,12 @@
 export default class Decoding {
-  constructor(text = "") {
-    this.text = text !== "" ? this.decode(text) : null;
+  constructor(text) {
+    this.text = text = this.decode(text);
   }
   decode(value) {
     const decodeStr = decodeURIComponent(
       atob(value)
         .split("")
-        .map(c => {
+        .map((c) => {
           return "%" + ("00" + c.charCodeAt(0).toString(16)).slice(-2);
         })
         .join("")
