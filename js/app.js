0910bf903304c50c03ded710495cfd331df91692
Jarosław Sochacki <inz.jaroslawsochacki@gmail.com>
2020-04-15 14:40:56 +0200

    Fixed a issueg that automatically encode text when the page was refreshed and the value remained in the textarea

--- js/app.js
+++ js/app.js
@@ -1,94 +1,82 @@
 import "../scss/index.scss";
-import base64 from "./base64";
-import removeDataFromSessionStorage from "./removeSession";
+import { base64, changeBtnContent } from "./base64";
+import handleButtonsMsg from "./handleButtonsMsg";
 import "@fortawesome/fontawesome-free/js/fontawesome";
 import "@fortawesome/fontawesome-free/js/solid";
 import "@fortawesome/fontawesome-free/js/regular";
 import "@fortawesome/fontawesome-free/js/brands";
 
 // global variables
+const btn = document.getElementById("btn");
+const radioBtn = document.querySelectorAll('input[name="radio_btn"]');
 const history = document.querySelector(".history");
 
 // check or get items from local session when browser will be refresh or loaded
 const getItems = () => {
-  base64();
+  const section = document.getElementsByTagName("section")[1];
 
-  // set if history content is empty on load
-  if (history.textContent === "")
-    history.innerHTML = '<h3 class="noData">There is nothing to display</h3>';
-};
+  // put all data from session storage to DOM
+  let data = "";
 
-// remove specific element from DOM and session storage
-const handleButtons = (e) => {
-  // check if clicked elemnt contains a delete class
-  if (e.target.classList.contains("delete")) {
-    // select current parent
-    const div = e.target.parentElement.parentElement.parentElement;
-    const parent = e.target.parentElement.parentElement;
-    // select current encode or decode message
-    const child = e.target.parentElement.previousElementSibling;
+  // local session encode array values
+  let source;
 
-    // set animations on a specific element
-    parent.style.animationName = "delete";
-    parent.style.animationDuration = ".3s";
-    parent.style.animationTimingFunction = "ease-out";
-
-    // remove element after animation will finsh
-    setTimeout(() => {
-      // remove from session storage
-      removeDataFromSessionStorage(child);
-      // remove from DOM
-      parent.remove();
-      // set if history content is empty
-      if (div.firstElementChild === null) {
-        history.innerHTML =
-          '<h3 class="noData">There is nothing to display</h3>';
-      }
-    }, 300);
+  // Check if any data is in session storage
+  if (sessionStorage.getItem("data") === null) {
+    source = [];
+  } else {
+    // get data from session storage and put in to variable
+    source = JSON.parse(sessionStorage.getItem("data"));
   }
 
-  if (e.target.classList.contains("copy")) {
-    // selected div text
-    const textToCopy = e.target.parentElement.previousElementSibling;
-    //check and see if the user had a text selection range
-    let currentRange;
-    //create a selection range
-    const copyRange = document.createRange();
+  // loop from source array to get data
+  source.forEach((source) => {
+    data += `
+      <div class="content ${source.class}">
+        <div class="content__message">${source.text}</div>
+        <div class="content__btn">
+          <div class="copy"> 
+            <i class="far fa-clipboard"></i>
+          </div>
+          <div class="delete">
+            <i class="fas fa-times"></i>
+          </div>
+        </div>
+      </div>
+      `;
+  });
 
-    if (document.getSelection().rangeCount > 0) {
-      //the user has a text selection range, store it
-      currentRange = document.getSelection().getRangeAt(0);
-      //remove the current selection
-      window.getSelection().removeRange(currentRange);
-    }
+  // inserting all data from local session as innerHTML in to the DOM
+  history.innerHTML = data;
 
-    //choose the element we want to select the text of
-    copyRange.selectNode(textToCopy);
-    //select the text inside the range
-    window.getSelection().addRange(copyRange);
-    //copy the text to the clipboard
-    document.execCommand("copy");
+  /* ----------------------------------------------- */
+  // this section only start when page is load, refresh or last element will be delete from session storage
+  const h3 = document.querySelector(".noData");
 
-    //remove our selection range
-    window.getSelection().removeRange(copyRange);
+  // set title if history content is empty on load
+  if (history.firstElementChild === null) {
+    //create this element when history is empty
+    const h3 = document.createElement("h3");
+    h3.setAttribute("class", "noData");
+    const text = "There is nothing to display";
 
-    //return the old selection range
-    if (currentRange) {
-      window.getSelection().addRange(currentRange);
-    }
-    // set animations on a specific element for copy text
-    textToCopy.style.animationName = "copy_text";
-    textToCopy.style.animationDuration = "1s";
-    textToCopy.style.animationTimingFunction = "linear";
+    h3.appendChild(document.createTextNode(text));
 
-    setTimeout(() => {
-      textToCopy.style = "";
-    }, 1000);
+    section.appendChild(h3);
+  } else if (h3) {
+    // if h3 exist remove it
+    h3.remove(h3);
   }
 };
 
 // load items from session if exist
 document.addEventListener("DOMContentLoaded", getItems);
 
-// remove data from DOM
-history.addEventListener("click", handleButtons);
+// handle buttons that allow to remove data and copy message
+history.addEventListener("click", handleButtonsMsg);
+
+// start encode or decode
+btn.addEventListener("click", base64);
+
+// select option
+[...radioBtn].map((item) => item.addEventListener("change", changeBtnContent));
--- js/base64.js
+++ js/base64.js
@@ -1,124 +1,52 @@
-import Encode from "./encoding";
-import Decode from "./decoding";
-import smoothScrollDown from "./scroll";
+import handleSelectOption from "./handleSelectOption";
 
-// global variables
-const btn = document.getElementById("btn");
-
-// main function that use class to encode and decode a string
-const base64 = () => {
+// this function return what option is selected from radio input
+const getSelectionValue = () => {
   const radioBtn = document.querySelectorAll('input[name="radio_btn"]');
-  const textareaValue = document.getElementById("form-group__textarea");
-  const history = document.querySelector(".history");
-  const textareaText = document.querySelector(".form-group__label");
+  let value;
 
-  // this function return what option is selected from radio input
-  const getSelectionValue = () => {
-    let value;
+  for (let i = 0; i < radioBtn.length; i++) {
+    if (radioBtn[i].checked) {
+      value = radioBtn[i].value;
 
-    for (let i = 0; i < radioBtn.length; i++) {
-      if (radioBtn[i].checked) {
-        value = radioBtn[i].value;
-
-        break;
-      }
+      break;
     }
+  }
 
-    return value;
-  };
+  return value;
+};
 
-  //
-  const selectOption = (option) => {
-    //set a class depend what option value is pass
-    let select;
+export const base64 = () => {
+  const h3 = document.querySelector(".noData");
 
-    // local session encode array values
-    let source;
-
-    // put all data from session storage to DOM
-    let data = "";
-
-    // check what option is pass in attribute
-    option === "encode"
-      ? (select = new Encode(textareaValue.value))
-      : (select = new Decode(textareaValue.value));
-
-    // Check if any data is in session storage
-    if (sessionStorage.getItem("data") === null) {
-      source = [];
-    } else {
-      // get data from session storage and put in to variable
-      source = JSON.parse(sessionStorage.getItem("data"));
-    }
-    // prevents to add empty string to array by pressing a button or refreshing a browser
-    if (textareaValue.value.length > 0) {
-      // add to array as a JSON object
-      source.push({ text: select.text, class: `${option}` });
-      // and scroll down to the bottom when message is under height: 100vh
-      smoothScrollDown("scrollTo", 1000);
-    }
-    // convert a value to a JSON string
-    sessionStorage.setItem("data", JSON.stringify(source));
-
-    // loop from source array to get data
-    source.forEach((source) => {
-      data += `
-      <div class="content ${source.class}">
-      <div class="content__message">${source.text}
-      </div>
-      <div class="content__btn">
-      <div class="copy"> 
-      <i class="far fa-clipboard"></i>
-      </div>
-      <div class="delete">
-      <i class="fas fa-times"></i>
-      </div>
-      </div>
-      </div>
-      `;
-    });
-
-    // inserting all data from local session as innerHTML to the DOM
-    history.innerHTML = data;
-
-    //clear textarea
-    textareaValue.value = "";
-  };
+  // if h3 element exists in history then delete it
+  if (h3) h3.remove();
 
   switch (true) {
     // do when radio encode button will pressed
     case getSelectionValue() === "encode":
-      selectOption("encode");
+      handleSelectOption("encode");
       break;
 
     // do when radio decode button will pressed
     case getSelectionValue() === "decode":
-      selectOption("decode");
+      handleSelectOption("decode");
       break;
   }
-  // change the text according to the selected option
-  const changeBtnContent = (e) => {
-    const getSelection = e.target.value;
-
-    switch (true) {
-      case getSelection == "encode":
-        btn.textContent = "Encode";
-        textareaText.textContent = "Encode the message";
-        break;
-      case getSelection == "decode":
-        btn.textContent = "Decode";
-        textareaText.textContent = "Decode the message";
-        break;
-    }
-  };
-
-  // select option
-  [...radioBtn].map((item) =>
-    item.addEventListener("change", changeBtnContent)
-  );
 };
+// change the lebel textarea title according to the selected option
+export const changeBtnContent = (e) => {
+  const textareaText = document.querySelector(".form-group__label");
+  const getSelection = e.target.value;
 
-// start encode or decode
-btn.addEventListener("click", base64);
-
-export default base64;
+  switch (true) {
+    case getSelection == "encode":
+      btn.textContent = "Encode";
+      textareaText.textContent = "Encode the message";
+      break;
+    case getSelection == "decode":
+      btn.textContent = "Decode";
+      textareaText.textContent = "Decode the message";
+      break;
+  }
+};
